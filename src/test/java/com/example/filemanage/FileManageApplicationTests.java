383eca355b955247ae02177d18cd8160e65beb63
杨骜捷 <1404670552@qq.com>
2024-01-03 16:37:08 +0800

    初始化

---
+++ src/test/java/com/example/filemanage/FileManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
